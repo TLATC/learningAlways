13c8bc0b49d253fe55eb1a07206faa0e2c70bd91
wxh <wuxiaohao@utry.cn>
2019-03-26 18:09:16 +0800

    feat: 增加mybatis和数据库配置（未测）

--- learningAlways/src/test/java/com/shawn/learningalways/example/controller/UserController.java
+++ learningAlways/src/test/java/com/shawn/learningalways/example/controller/UserController.java
@@ -107,6 +107,6 @@ public class UserController {
     }
 
     public static void main(String[] args) {
-        new Thread( () -> System.out.println("In Java8, Lambda expression rocks !!") ).start();
+//        new Thread( () -> System.out.println("In Java8, Lambda expression rocks !!") ).start();
     }
 }
