1be0593242ec873fadc522ee1ac5f10cc2b306ca
wxh <wuxiaohao@utry.cn>
2019-03-28 13:16:26 +0800

    feat: 新增ApplicationRunner，容器加载完成后运行

---
+++ learningAlways/src/main/java/com/shawn/learningalways/base/MyApplicationRunner.java
@@ -0,0 +1,30 @@
+package com.shawn.learningalways.base;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * @ClassName: MyApplicationRunner
+ * @Description 执行时机为容器启动完成
+ * @author: Shawn Wu
+ * @date: 2019/3/28 13:06
+ * @version:
+ */
+@Component
+public class MyApplicationRunner  implements ApplicationRunner {
+
+    /**
+     * @Description 容器启动完成后执行
+     *              已经可以调用service层方法
+     * @param applicationArguments
+     * @return
+     * @date 2019/3/28 13:12
+     * @auther Shawn Wu
+     */
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+        System.out.println("容器加载已完成======");
+    }
+}
