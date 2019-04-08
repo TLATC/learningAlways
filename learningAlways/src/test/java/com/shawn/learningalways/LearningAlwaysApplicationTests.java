6785dbbc0a7db7b9f9b4728c81cd3b62288c4e8e
Shawn Wu <w1432230275@163.com>
2019-04-08 21:22:55 +0800

    test：增加mockmvc，发get请求测试/hello

--- learningAlways/src/test/java/com/shawn/learningalways/LearningAlwaysApplicationTests.java
+++ learningAlways/src/test/java/com/shawn/learningalways/LearningAlwaysApplicationTests.java
@@ -1,16 +1,60 @@
 package com.shawn.learningalways;
 
+import com.shawn.learningalways.example.controller.ExampleController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LearningAlwaysApplicationTests {
 
+	/**
+	 * MockMvc实现了对Http请求的模拟，但不依赖于网络环境
+	 */
+	private MockMvc mvc;
+
+	/**
+	 * MockMvc初始化方法
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception{
+		// 通过MockMvcBuilder构造MockMvc的构造器
+		mvc = MockMvcBuilders.standaloneSetup(new ExampleController()).build();
+	}
+
+	/**
+	 * @Description 模拟http请求，测试ExamoleController中的hello方法
+	 * @param
+	 * @return
+	 * @date 2019/4/8 21:14
+	 * @author Shawn Wu
+	 */
+	@Test
+	public void getHello() throws Exception{
+		/*
+		 * 调用perform，执行一个RequestBuilder请求，调用controller的业务处理逻辑
+		 */
+		// 模拟get请求，访问/hello
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+				// 请求完成后的断言，使用StatusResultMatchers对请求结果进行验证
+				.andExpect(status().isOk())
+				// 使用ContentResultMatchers对请求返回的内容进行验证
+				.andExpect(content().string(equalTo("Hello World!")));
+	}
+
+
 	@Test
 	public void contextLoads() {
 	}
-
 }
