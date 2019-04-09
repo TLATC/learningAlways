1ad00f741d21ec8771a970886c857292ac5d3104
Shawn Wu <w1432230275@163.com>
2019-04-09 21:48:58 +0800

    style: 增加注释

--- learningAlways/src/main/java/com/shawn/learningalways/example/controller/UserController.java
+++ learningAlways/src/main/java/com/shawn/learningalways/example/controller/UserController.java
@@ -85,6 +85,7 @@ public class UserController {
             @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
     })
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    // 使用@ModelAttribute注解的实体类接收前端发来的数据格式需要为"x-www-form-urlencoded"，@RequestBody注解的实体类接收前端的数据格式为JSON(application/json)格式。
     public String putUser(@PathVariable Long id, @RequestBody User user){
         User u = userMap.get(id);
         u.setName(user.getName());
