633080186ed99da1df37004307094d4f9ff6b23d
wxh <wuxiaohao@utry.cn>
2019-05-08 16:09:21 +0800

    feat: 文件改为上传到本地服务器uploadDir目录下

--- learningAlways/src/main/java/com/shawn/learningalways/example/controller/FileOptController.java
+++ learningAlways/src/main/java/com/shawn/learningalways/example/controller/FileOptController.java
@@ -1,6 +1,8 @@
 package com.shawn.learningalways.example.controller;
 
 import com.shawn.learningalways.base.controller.BaseController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 
@@ -23,10 +26,12 @@ import java.io.IOException;
 @RequestMapping("/fileOpt")
 public class FileOptController extends BaseController{
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileOptController.class);
+
     /**
      * 文件保存路径
      */
-    String filePath = "C:/fileTest";
+    private String uploadDir = "uploadDir";
 
     /**
      * @Description 访问upload.html，文件上传页面
@@ -50,7 +55,7 @@ public class FileOptController extends BaseController{
      * @auther Shawn Wu
      */
     @PostMapping("/upload")
-    public String upload(@RequestParam("file")MultipartFile file, ModelMap map){
+    public String upload(@RequestParam("file")MultipartFile file, ModelMap map, HttpServletRequest request){
         // 加入一个属性，模板中可以读取该属性。默认上传失败
         map.addAttribute("result", "文件上传失败");
 
@@ -60,8 +65,9 @@ public class FileOptController extends BaseController{
         }
         // 文件名
         String fileName = file.getOriginalFilename();
-        // 文件大小
-        int size = (int)file.getSize();
+        // 临时文件目录
+        String filePath = request.getSession().getServletContext().getRealPath("")+ "/" + uploadDir;
+        LOGGER.debug("上传文件的临时目录是：", filePath);
 
         File dest = new File(filePath + "/" + fileName);
         // 如果父目录不存在，则创建
