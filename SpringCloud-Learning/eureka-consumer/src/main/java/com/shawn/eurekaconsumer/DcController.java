dffa3c200c5c0cad1fe763bae3782f6dac7b9092
wxh <wuxiaohao@utry.cn>
2019-09-23 18:16:10 +0800

    feat：新建服务消费者工程

---
+++ SpringCloud-Learning/eureka-consumer/src/main/java/com/shawn/eurekaconsumer/DcController.java
@@ -0,0 +1,41 @@
+package com.shawn.eurekaconsumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @ClassName: DcController
+ * @Description 服务消费控制层
+ * @author: Shawn Wu
+ * @date: 2019/9/23 17:42
+ * @version:
+ */
+@RestController
+public class DcController {
+
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+    @Autowired
+    RestTemplate restTemplate;
+
+    /**
+     * @Description 消费服务
+     * @param
+     * @return
+     * @date 2019/9/23 17:58
+     * @auther Shawn Wu
+     */
+    @GetMapping("/consumer")
+    public String dc(){
+        // 负载均衡的选出一个eureka-client的服务实例
+        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+        // 拼接出接口url
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/eclient/dc";
+        System.out.println("要调用的url是：" + url);
+        return  restTemplate.getForObject(url, String.class);
+    }
+}
---
+++ SpringCloud-Learning/eureka-consumer/src/main/java/com/shawn/eurekaconsumer/EurekaConsumerApplication.java
@@ -0,0 +1,29 @@
+package com.shawn.eurekaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaConsumerApplication {
+
+	/**
+	 * @Description 初始化RestTemplate
+	 * @param
+	 * @return
+	 * @date 2019/9/23 17:41
+	 * @auther Shawn Wu
+	 */
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaConsumerApplication.class, args);
+	}
+
+}
