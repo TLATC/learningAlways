0cd1dfde3f953f2a7552e6fa2b0da6ad3bb926a3
wxh <wuxiaohao@utry.cn>
2019-10-05 10:50:47 +0800

    feat：使用fegin进行调用

--- SpringCloud-Learning/eureka-consumer/src/main/java/com/shawn/eurekaconsumer/DcController.java
+++ SpringCloud-Learning/eureka-consumer/src/main/java/com/shawn/eurekaconsumer/DcController.java
@@ -21,6 +21,8 @@ public class DcController {
     LoadBalancerClient loadBalancerClient;
     @Autowired
     RestTemplate restTemplate;
+    @Autowired
+    DcFeignClient dcFeignClient;
 
     /**
      * @Description 消费服务
@@ -31,14 +33,54 @@ public class DcController {
      */
     @GetMapping("/consumer")
     public String dc(){
-        /// 用loadBalancerClient负载均衡的选出一个eureka-client的服务实例
-//        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
-        // 拼接出接口url
-//        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/eclient/dc";
+        // 使用loadBalancerClient调用
+//        return loadBalancerConsumer();
 
+        // 使用Ribbon调用
+//        return RibbonConsumer();
+
+        // 使用fegin调用
+        return  feginConsumer();
+    }
+
+    /**
+     * @Description 使用loadBalancerClient调用
+     * @param
+     * @return
+     * @date 2019/10/5 10:29
+     * @auther Shawn Wu
+     */
+    private String loadBalancerConsumer(){
+        // 用loadBalancerClient负载均衡的选出一个eureka-client的服务实例
+        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+        // 拼接出接口url
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/eclient/dc";
+        System.out.println("要调用的url是：" + url);
+        return  restTemplate.getForObject(url, String.class);
+    }
+
+    /**
+     * @Description 使用Ribbon调用
+     * @param
+     * @return 
+     * @date 2019/10/5 10:31
+     * @auther Shawn Wu
+     */
+    private String RibbonConsumer(){
         // 用服务名，而不用ip和端口
         String url = "http://eureka-client/eclient/dc";
         System.out.println("要调用的url是：" + url);
         return  restTemplate.getForObject(url, String.class);
+    }
+
+    /**
+     * @Description 使用fegin调用
+     * @param
+     * @return 
+     * @date 2019/10/5 10:16
+     * @auther Shawn Wu
+     */
+    private String feginConsumer(){
+        return dcFeignClient.feginConsumer();
     }
 }
---
+++ SpringCloud-Learning/eureka-consumer/src/main/java/com/shawn/eurekaconsumer/DcFeignClient.java
@@ -0,0 +1,25 @@
+package com.shawn.eurekaconsumer;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @ClassName: DcFeignClient
+ * @Description Feign的客户端接口定义
+ * @author: Shawn Wu
+ * @date: 2019/10/5 10:10
+ * @version:
+ */
+@FeignClient("eureka-client")
+public interface DcFeignClient {
+
+    /**
+     * @Description 调用eureka-client服务的dc接口
+     * @param
+     * @return
+     * @date 2019/10/5 10:12
+     * @auther Shawn Wu
+     */
+    @GetMapping("/eclient/dc")
+    String feginConsumer();
+}
